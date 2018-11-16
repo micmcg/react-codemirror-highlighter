7eff9492bee21d04d943e140699f0bb698704c05
Michael McGlynn <mmcglynn@atlassian.com>
2018-11-16 16:46:12 +1100

    WIP

--- src/codemirror-highlight.js
+++ src/codemirror-highlight.js
@@ -1,7 +1,13 @@
 import 'codemirror/mode/meta';
-import 'codemirror/mode/javascript/javascript';
 import CodeMirror from './codemirror-shim';
 import 'codemirror/theme/monokai.css';
 
-export const highlight = (source, language, el) =>
-    CodeMirror.runMode(source, CodeMirror.findModeByName(language).mode, el);
+export const highlight = (source, language, el) => {
+    try {
+        CodeMirror.runMode(source, CodeMirror.findModeByName(language).mode, el);
+    } catch (e) {
+        import(`codemirror/mode/${language}/${language}`).then(() => {
+            CodeMirror.runMode(source, CodeMirror.findModeByName(language).mode, el);
+        });
+    }
+};
